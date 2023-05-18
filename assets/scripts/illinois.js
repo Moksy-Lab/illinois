00aeff51b39c21dbc9d394c58c98b573c4109fdf
627md-Tajul-Islam <tajul.azazil@gmail.com>
2023-05-18 22:10:14 +0600

    code.js

--- assets/scripts/illinois.js
+++ assets/scripts/illinois.js
@@ -1,3 +1,13 @@
-function changeColor() {
-    document.getElementById('myText').style.color = 'red';
+function addAlertToButton() {
+    const button = document.querySelector('button.btn.btnReg');
+    
+    if (button) {
+      button.addEventListener('click', function() {
+        alert('Button clicked!');
+      });
+    } else {
+      console.log('No button with the classes "btn btnReg" found.');
+    }
   }
+  
+  addAlertToButton();
